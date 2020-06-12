6be8eeca443f4e8b846df023b59b7866efa0f721
ajaidanial <ajai@geopits.com>
2020-06-13 00:01:37 +0530

    drag drop init and added configurations

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import AppDragDropDemo from './AppDragDropDemo'
+
+import './index.css'
+
+ReactDOM.render(<AppDragDropDemo />, document.getElementById('root'))
